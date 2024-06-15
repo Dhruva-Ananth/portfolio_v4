6dbe1f1b9aba8b960a13ea63836f0b249496af1f
Dhruva Anantha Datta <60318299+Dhruva-Ananth@users.noreply.github.com>
2024-06-15 22:23:44 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'ananthadattadhruva@gmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/Dhruva-Ananth',
     },
     {
       name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
+      url: 'https://www.instagram.com/1add4n',
     },
     {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
+      name: 'X',
+      url: 'https://x.com/DhruvaAnantha',
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/ananthadattad',
     },
     {
       name: 'Codepen',
